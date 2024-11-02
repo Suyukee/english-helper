0014af03d5c2f082ab53c1c56c507694990d53fa
Suyukee <diefuck@ya.ru>
2024-11-03 02:42:47 +0300

    Исправил ошибки

--- src/api/words.ts
+++ src/api/words.ts
@@ -1,6 +1,7 @@
 import { A1 } from '@/api/words-data/a1';
 import { A2 } from '@/api/words-data/a2';
 import { B1 } from '@/api/words-data/b1';
+import { B2 } from './words-data/b2';
 
 export const words = {
 	A1: A1,
@@ -9,7 +10,7 @@ export const words = {
 
 	B1: B1,
 
-	B2: [],
+	B2: B2,
 
 	C1: [],
 
--- src/app/b2/page.tsx
+++ src/app/b2/page.tsx
@@ -2,7 +2,7 @@ import { getAnwerOptions } from '@/api/get-words';
 import LevelPage from '@/components/level-page';
 
 export default async function BeginnerPage() {
-	const answerOptions = await getAnwerOptions('B1');
+	const answerOptions = await getAnwerOptions('B2');
 
 	return <LevelPage answerOptions={answerOptions} />;
 }
