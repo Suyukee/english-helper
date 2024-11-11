6058b2fd9a0cca148ad78512c2f8f59959a626b2
Suyukee <diefuck@ya.ru>
2024-11-11 16:44:33 +0300

    Добавил для теста немного C2

--- src/api/words-data/c2.ts
+++ src/api/words-data/c2.ts
@@ -1,1 +1,9 @@
-export const C2 = [];
+export const C2 = [
+	{ eng: 'kerb', rus: 'бордюр / ограничение' },
+	{ eng: 'follower', rus: 'последователь' },
+	{ eng: 'dim', rus: 'тусклый / слабый' },
+	{ eng: 'hollow', rus: 'полый / пустой / глухой (звук)' },
+	{ eng: 'wicked', rus: 'злой / безнравственный' },
+	{ eng: 'dazed', rus: 'ошеломленный / обалделый' },
+	{ eng: 'instinct', rus: 'инстинкт' },
+];
