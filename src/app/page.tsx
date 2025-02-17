998c81e56cfc402dc9faafb3c226ec2a627ade5a
suyukee <suyukee@ya.ru>
2025-02-17 17:03:18 +0300

    Revert "Создал проект"
    This reverts commit 77aa3bcb05b4ff598fdc2b7e6a5d435a7dc9021b.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,77 @@
-import Link from 'next/link';
-import { words } from '@/api/words';
-import { clearIsGuess } from '@/api/get-words';
-import styles from '@/styles/main-page.module.css';
-
-const englishLevel = [
-	{ title: `A1 — Beginner (+${words.A1.length} слов)`, href: '/a1' },
-	{ title: `A2 — Elementary (+${words.A2.length} слов)`, href: '/a2' },
-	{ title: `B1 — Intermediate (+${words.B1.length} слов)`, href: '/b1' },
-	{ title: `B2 — Upper Intermediate (+${words.B2.length} слов)`, href: '/b2' },
-	{ title: `C1 — Advanced (+${words.C1.length} слов)`, href: '/c1' },
-	{ title: `C2 — Proficiency (+${words.C2.length} слов)`, href: '/c2' },
-];
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
-	clearIsGuess();
-
 	return (
 		<div className={styles.page}>
 			<main className={styles.main}>
-				<div>
-					<h1>English helper</h1>
-					<p>Выберете ваш уровень</p>
+				<Image
+					className={styles.logo}
+					src="/next.svg"
+					alt="Next.js logo"
+					width={180}
+					height={38}
+					priority
+				/>
+				<ol>
+					<li>
+						Get started by editing <code>src/app/page.tsx</code>.
+					</li>
+					<li>Save and see your changes instantly.</li>
+				</ol>
+
+				<div className={styles.ctas}>
+					<a
+						className={styles.primary}
+						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Image
+							className={styles.logo}
+							src="/vercel.svg"
+							alt="Vercel logomark"
+							width={20}
+							height={20}
+						/>
+						Deploy now
+					</a>
+					<a
+						href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+						target="_blank"
+						rel="noopener noreferrer"
+						className={styles.secondary}
+					>
+						Read our docs
+					</a>
 				</div>
-				<ul className={styles.listBox}>
-					{englishLevel.map((lvl, i) => (
-						<li key={i} className={styles.listItem}>
-							<Link href={lvl.href} className={styles.secondary}>
-								{lvl.title}
-							</Link>
-						</li>
-					))}
-				</ul>
 			</main>
+			<footer className={styles.footer}>
+				<a
+					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
+					Learn
+				</a>
+				<a
+					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
+					Examples
+				</a>
+				<a
+					href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
+					Go to nextjs.org →
+				</a>
+			</footer>
 		</div>
 	);
 }
