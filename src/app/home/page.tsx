cd4c8b850cacc6cf66e173303c40f540e546f6d0
suyukee <suyukee@ya.ru>
2025-02-18 16:32:39 +0300

    Проверка

---
+++ src/app/home/page.tsx
@@ -0,0 +1,23 @@
+import { createClient } from '@/shared/lib/server';
+
+export default async function HomePage() {
+	const supabase = await createClient();
+
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+
+	const { data, error } = await supabase.from('words').select().eq('id', user?.id);
+
+	if (error) return <div>Error</div>;
+
+	return (
+		<div>
+			<center>
+				<h3>User ID - {user?.id}</h3>
+				<br />
+				<pre>{JSON.stringify(data)}</pre>
+			</center>
+		</div>
+	);
+}
