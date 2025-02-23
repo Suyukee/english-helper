2d3895fe6a3ec75db650c159e4cda770ee31ce4c
suyukee <suyukee@ya.ru>
2025-02-23 09:56:14 +0300

    Добавил рефреш страницы после сброса

--- src/widgets/reset-progress/ResetProgress.tsx
+++ src/widgets/reset-progress/ResetProgress.tsx
@@ -1,12 +1,17 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { resetProgress } from './actions';
 import ResetIcon from '@/shared/icons/ResetIcon';
 import styles from '@/shared/styles/reset-progress.module.css';
 
 export default function ResetProgress() {
+	const router = useRouter();
+
 	const handleReset = async () => {
 		await resetProgress();
+
+		router.refresh();
 	};
 
 	return (
