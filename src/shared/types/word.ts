92b2a7d8b781fa227a2943e9cc58c1f2ff2b6c7d
suyukee <suyukee@ya.ru>
2025-02-25 13:28:41 +0300

    Немного изменил типы и массив levels

--- src/shared/types/word.ts
+++ src/shared/types/word.ts
@@ -1,8 +1,21 @@
-export type WordLevel = 'A1' | 'A2' | 'B1' | 'B2' | 'C1' | 'C2';
+export enum WordLevel {
+	A1 = 'A1',
+	A2 = 'A2',
+	B1 = 'B1',
+	B2 = 'B2',
+	C1 = 'C1',
+	C2 = 'C2',
+}
 
 export interface Word {
 	id: string;
 	eng: string;
 	rus: string;
 	level: WordLevel;
+}
+
+export interface WordLevelProgress {
+	lvl: WordLevel;
+	learned: number;
+	total: number;
 }
--- src/widgets/level-list/LevelList.tsx
+++ src/widgets/level-list/LevelList.tsx
@@ -1,16 +1,10 @@
 import Link from 'next/link';
 import { getAllProgress } from './actions';
-import { WordLevel } from '@/shared/types/word';
+import { WordLevel, WordLevelProgress } from '@/shared/types/word';
 import styles from '@/shared/styles/level-list.module.css';
 
 interface LevelListProps {
 	userId: string;
-}
-
-interface WordLevelProgress {
-	lvl: WordLevel;
-	learned: number;
-	total: number;
 }
 
 export default async function LevelList({ userId }: LevelListProps) {
@@ -22,12 +16,12 @@ export default async function LevelList({ userId }: LevelListProps) {
 	};
 
 	const levels = [
-		{ title: `A1 — Beginner (${getLevelProgress('A1')} слов)`, url: '/A1' },
-		{ title: `A2 — Elementary (${getLevelProgress('A2')})`, url: '/A2' },
-		{ title: `B1 — Intermediate (${getLevelProgress('B1')} слов)`, url: '/B1' },
-		{ title: `B2 — Upper Intermediate (${getLevelProgress('B2')} слов)`, url: '/B2' },
-		{ title: `C1 — Advanced (${getLevelProgress('C1')} слов)`, url: '/C1' },
-		{ title: `C2 — Proficiency (${getLevelProgress('C2')} слов)`, url: '/C2' },
+		{ level: WordLevel.A1, name: 'Begginer' },
+		{ level: WordLevel.A2, name: 'Elementary' },
+		{ level: WordLevel.B1, name: 'Intermediate' },
+		{ level: WordLevel.B2, name: 'Upper Intermediate' },
+		{ level: WordLevel.C1, name: 'Advanced' },
+		{ level: WordLevel.C2, name: 'Proficiency' },
 	];
 
 	if (!userId) return;
@@ -35,8 +29,8 @@ export default async function LevelList({ userId }: LevelListProps) {
 	return (
 		<nav className={styles.nav}>
 			{levels.map((item) => (
-				<Link className={styles.link} href={item.url} key={item.url}>
-					{item.title}
+				<Link className={styles.link} href={`/${item.level}`} key={item.level}>
+					{item.level} - {item.name}: {getLevelProgress(item.level)}
 				</Link>
 			))}
 		</nav>
